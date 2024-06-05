6b2ca432cb9621547e5a4075918b18aaac6ba097
mohamedalimohamed1 <mazcuud37@gmail.com>
2024-06-05 23:40:17 +0300

    Fix asset paths and deploy

--- src/components/landingPage/NavbarSec/navbar.jsx
+++ src/components/landingPage/NavbarSec/navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+// src\components\landingPage\NavbarSec\navbar.jsx
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-scroll';
-import './navbar.css';
 import Logo from '../../../assets/erenAzedLogo.png';
+import './navbar.css';
 
 const Navbar = () => {
+  const [showMessage, setShowMessage] = useState(false);
+  const girisRef = useRef(null);
+
+  useEffect(() => {
+    // Access the element with the id 'giris'
+    const girisElement = girisRef.current;
+    console.log(girisElement); // Perform any action with the element
+  }, []);
+
+  const handleGirisClick = () => {
+    setShowMessage(true);
+    alert('Bu hizmet en yakında size sunulacak');
+  };
+
   return (
     <div className='navbar'>
       <div className='logo-sec'>
@@ -15,7 +30,7 @@ const Navbar = () => {
           <li><Link to="Languages" smooth={true} duration={500} offset={-80}>DİLLER</Link></li>
           <li><Link to="Testimonials" smooth={true} duration={500} offset={-100}>YORUMLAR</Link></li>
           <li><Link to="Contacts" smooth={true} duration={500} offset={-70}>İLETİŞİM</Link></li>
-          <li className='login'>GİRİŞ YAP</li>
+          <li id='giris' className='login' ref={girisRef} onClick={handleGirisClick}>GİRİŞ YAP</li>
         </ul>
       </div>
     </div>
